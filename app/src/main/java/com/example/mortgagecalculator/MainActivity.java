9a759625ca22afc518536baa8f3e95c49e7bf053
greg-navarro <gregorylitmannavarro@gmail.com>
2021-09-13 11:33:03 -0700

    Connect the activity models to one another with Intent

--- app/src/main/java/com/example/mortgagecalculator/MainActivity.java
+++ app/src/main/java/com/example/mortgagecalculator/MainActivity.java
@@ -2,15 +2,25 @@ package com.example.mortgagecalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button modifyDataButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-        setContentView(R.layout.activity_data);
+        setContentView(R.layout.activity_main);
+
+        modifyDataButton = findViewById(R.id.modify_data);
+    }
+
+    public void modifyData(View v) {
+        Intent myIntent = new Intent(this, DataActivity.class);
+        this.startActivity(myIntent);
 
     }
 }
