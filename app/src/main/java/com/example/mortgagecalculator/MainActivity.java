531767b1199a8109a15bdadb37d172ccb36e3a8f
toanos <toan.ha1896@gmail.com>
2021-09-13 21:08:04 -0700

    AndroidManifest: change app theme

--- app/src/main/java/com/example/mortgagecalculator/MainActivity.java
+++ app/src/main/java/com/example/mortgagecalculator/MainActivity.java
@@ -9,13 +9,13 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button modifyDataButton;
+//    private Button modifyDataButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        modifyDataButton = findViewById(R.id.modify_data_button);
+//        modifyDataButton = findViewById(R.id.modify_data_button);
     }
 
     public void modifyData(View v) {
