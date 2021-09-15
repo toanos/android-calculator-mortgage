6ee3ce4d42e77d15a546560b4bd1020fbac0db66
toanos <toan.ha1896@gmail.com>
2021-09-14 21:36:52 -0700

    activity_data.xml: remove unused constraint

--- app/src/main/java/com/example/mortgagecalculator/DataActivity.java
+++ app/src/main/java/com/example/mortgagecalculator/DataActivity.java
@@ -2,6 +2,8 @@ package com.example.mortgagecalculator;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,6 +13,19 @@ public class DataActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data);
+    }
+
+    public void updateView() {
+        Mortgage mortgage = MainActivity.mortgage;
+        if (mortgage.getYears() == 10) {
+            RadioButton radioButton10 = (RadioButton) findViewById(R.id.ten);
+            radioButton10.setChecked(true);
+        } else if (mortgage.getYears() == 15) {
+            RadioButton radioButton15 = (RadioButton) findViewById(R.id.fifteen);
+            radioButton15.setChecked(true);
+        } // 30 years is checked by default
+
+//        EditText amountEditText = (EditText) findViewById(R.id.amount);
     }
 
     public void goBack(View V){
