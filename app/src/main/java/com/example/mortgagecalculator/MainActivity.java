08b67bdb4c957a5a4119a062297f09392d661194
toanos <toan.ha1896@gmail.com>
2021-09-14 21:28:07 -0700

    MainActivity.java: implement updateView()

--- app/src/main/java/com/example/mortgagecalculator/MainActivity.java
+++ app/src/main/java/com/example/mortgagecalculator/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +16,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         mortgage = new Mortgage();
         setContentView(R.layout.activity_main);
+    }
+
+    public void onStart() {
+        super.onStart();
+        updateView();
+    }
+
+    public void updateView() {
+        TextView amountTextView = (TextView) findViewById(R.id.amount);
+        amountTextView.setText(mortgage.getFormattedAmount());
+        TextView yearsTextView = (TextView) findViewById(R.id.years);
+        yearsTextView.setText("" + mortgage.getYears());
+        TextView rateTextView = (TextView) findViewById(R.id.rate);
+        rateTextView.setText("" + mortgage.getRate());
     }
 
     public void modifyData(View v) {
