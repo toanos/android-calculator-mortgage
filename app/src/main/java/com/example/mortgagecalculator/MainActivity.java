1c185ba2acddbdb0a3ebb5c2662a8ef5c16aea26
greg-navarro <gregorylitmannavarro@gmail.com>
2021-09-15 10:32:11 -0700

    Add methods

--- app/src/main/java/com/example/mortgagecalculator/MainActivity.java
+++ app/src/main/java/com/example/mortgagecalculator/MainActivity.java
@@ -6,16 +6,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    public static Mortgage mortgage;
     private Button modifyDataButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mortgage = new Mortgage();
         setContentView(R.layout.activity_main);
 
         modifyDataButton = findViewById(R.id.modify_data);
+    }
+    public void onStart() {
+        super.onStart();
+        updateView();
+    }
+
+    public void updateView() {
+        TextView amountTV = (TextView) findViewById(R.id.amount);
+        amountTV.setText( mortgage.getFormattedAmount() );
+        TextView yearsTV = ( TextView ) findViewById( R.id.years );
+        yearsTV.setText("" + mortgage.getYears() );
+//        TODO complete
     }
 
     public void modifyData(View v) {
